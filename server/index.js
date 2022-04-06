724331de7ea84936e8a2aac9c57579441f7362c4
Eliad Degabli <eliadegabli@gmail.com>
2022-04-06 21:20:38 +0300

    change start p.j

--- server/index.js
+++ server/index.js
@@ -66,3 +66,13 @@ app.post("/api/delete", (req,res) =>{
 app.listen(5000,() => {
     console.log("run on port 3001");
 });
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
