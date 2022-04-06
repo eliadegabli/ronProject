72054316d3b8cb4f77aba50cf9b71b204ebc916c
Eliad Degabli <eliadegabli@gmail.com>
2022-04-06 22:29:19 +0300

    try deploy react + node js

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('build'));
-    app.get('*', (req,res) => {
-        req.sendFile(path.resolve(__dirname,'build', 'index.html'));
+    app.get('/', (req,res) => {
+        req.sendFile(path.join(__dirname+'/index.html'));
     })
 }
 
