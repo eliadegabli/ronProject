ac7b52e057196e8ff4eb49142b48048fda5ecbb7
Eliad Degabli <eliadegabli@gmail.com>
2022-04-06 22:39:26 +0300

    updae index.js server

--- server/index.js
+++ server/index.js
@@ -73,7 +73,7 @@ app.listen(5000,() => {
     console.log("run on port 5000");
 });
 
-app.set('port', (process.env.PORT || 5000));
+/*app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
@@ -81,4 +81,4 @@ app.get('/', function(request, response) {
     response.send(result);
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
-});
+});*/
